0e0e482ee257e1cb223a2c7c93be32d7a1671ae9
mi <m.ma.mai.mais@gmail.com>
2021-06-23 01:59:30 +0900

    調整

--- hoshikake.js
+++ hoshikake.js
@@ -1,12 +1,23 @@
-$(function(){
-  $(window).scroll(function (){
-    $('.effect-fade').each(function(){
-      var elemPos = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > elemPos - windowHeight){
-          $(this).addClass('effect-scroll');
-      }
-    });
-  });
-});
+/* 到達したら要素を表示させる */
+function showElementAnimation() {
+                    
+  var element = document.getElementsByClassName('fade-block');
+  if(!element) return; // 要素がなかったら処理をキャンセル
+                      
+  var showTiming = window.innerHeight > 768 ? 200 : 80; // 要素が出てくるタイミングはここで調整
+  var scrollY = window.pageYOffset; //スクロール量を取得
+  var windowH = window.innerHeight; //ブラウザウィンドウのビューポート(viewport)の高さを取得
+                    
+  for(var i=0;i<element.length;i++) { 
+    var elemClientRect = element[i].getBoundingClientRect(); 
+    var elemY = scrollY + elemClientRect.top; 
+    if(scrollY + windowH - showTiming > elemY) {
+      element[i].classList.add('is-show');
+    } else if(scrollY + windowH < elemY) {
+    // 上にスクロールして再度非表示にする場合はこちらを記述
+      element[i].classList.remove('is-show');
+    }
+  }
+}
+showElementAnimation();
+window.addEventListener('scroll', showElementAnimation);
